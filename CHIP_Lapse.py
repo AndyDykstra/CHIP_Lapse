5a7450e2e2d85aed46b1ce40b18b1c6bb598f4dd
AndyDykstra <38434378+AndyDykstra@users.noreply.github.com>
2018-04-16 11:05:00 -0500

    Create CHIP_Lapse.py

---
+++ CHIP_Lapse.py
@@ -0,0 +1,22 @@
+# import stuff
+
+# gphoto detect camera, print results
+
+# select start position
+# select start rotation
+
+# select end position
+# select end rotation
+
+# duration of timelapse
+# number of shots
+
+# preview y/n (switch to video, record, move)
+
+# create saved file with array of commands [delay, travel x, rotate y]
+
+# wait for begin
+
+# main FUNCTION - move, shoot, delay
+# check for limit switch
+# print status & percentage
